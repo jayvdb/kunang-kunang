955144876a301182a4ac084fd190ce5a1eddf126
John Vandenberg <jayvdb@gmail.com>
2018-02-28 10:22:41 +0700

    deploy.sh: Fail on errors

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
-set -E
+#!/bin/bash
+set -ex
+
+: "${JEKYLL_ARGS:=--incremental --strict_front_matter}"
+
 rm -rf _site
 npm run build
 yarn install
 cp node_modules/netlify-cms/dist/cms.js admin/cms.js
 cp node_modules/netlify-cms/dist/cms.css admin/cms.css
-jekyll build
+jekyll build ${JEKYLL_ARGS}
